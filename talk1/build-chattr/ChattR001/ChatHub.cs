a8fb43a63ac638d53f294253fb15d0c3bfbb2154
Damian Edwards <damian@damianedwards.com>
2020-01-28 18:01:16 +0000

    Add ChattR solution

---
+++ talk1/build-chattr/ChattR001/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChattR001
+{
+    public class ChatHub : Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.SendAsync("Send", message);
+        }
+
+        public async IAsyncEnumerable<DateTime> Stream()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return DateTime.UtcNow;
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
