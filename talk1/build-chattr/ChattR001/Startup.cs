b7a728c021b3f975c03f41df529ee5c9921cdf27
Damian Edwards <damian@damianedwards.com>
2020-01-29 09:56:33 +0000

    Add static HTML version of chat page

--- talk1/build-chattr/ChattR001/Startup.cs
+++ talk1/build-chattr/ChattR001/Startup.cs
@@ -37,7 +37,6 @@ namespace ChattR001
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
